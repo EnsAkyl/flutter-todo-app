094e38bd093806d94a03c23cfe73fce45cedfaad
Enes AKYOL <akyolenes77@gmail.com>
2025-05-13 21:53:34 +0300

    feat: Uygulama theme dosyası oluşturuldu.

--- lib/app/todo_app.dart
+++ lib/app/todo_app.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app/screens/home_screen.dart';
+import '../config/theme/app_theme.dart';
 
 class TodoApp extends StatelessWidget {
-  const TodoApp({super.key})
+  const TodoApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen()
+      theme: AppTheme.light,
+      home: const HomeScreen()
     );
   }
 }
