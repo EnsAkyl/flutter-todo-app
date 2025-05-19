28825fb158f7464a778fb535a9f35bf7cad0682d
Enes AKYOL <akyolenes77@gmail.com>
2025-05-19 18:41:25 +0300

    feat: Repository'ler oluşturuldu.

---
+++ lib/data/repositories/task_repositories.dart
@@ -0,0 +1,11 @@
+import 'package:todo_app/data/data.dart';
+
+abstract class TaskRepository {
+  Future<void> createTask(Task task);
+
+  Future<void> deleteTask(Task task);
+
+  Future<void> updateTask(Task task);
+
+  Future<List<Task>> getAllTasks();
+}
---
+++ lib/providers/task/task_notifier.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../data/data.dart';
+import '../providers.dart';
+
+class TaskNotifier extends StateNotifier<TaskState> {
+  final TaskRepository _repository;
+
+  TaskNotifier(this._repository) : super(const TaskState.initial());
+
+  Future<void> createTask(Task task) async {
+    try {
+      await _repository.createTask(task);
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+  }
+
+  Future<void> updateTask(Task task) async {
+    try {
+      final isComplete = !task.isCompleted;
+      final updateTask = task.copyWith(isCompleted: isComplete);
+      await _repository.updateTask(updateTask);
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+  }
+
+  Future<void> getTasks() async {
+    try {
+      final tasks = await _repository.getAllTasks();
+      state = state.copyWith(tasks: tasks);
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+  }
+
+  Future<void> deleteTask(Task task) async {
+    try {
+      await _repository.deleteTask(task);
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+  }
+}
